fd409d98aaea0cbab435bc266660ca1f4dfe6c2d
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-16 11:03:07 -0300

    refactor: change var name acessToken to accessToken

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -42,7 +42,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
         email: state.email,
         password: state.password
       })
-      localStorage.setItem('acessToken', account.acessToken)
+      localStorage.setItem('accessToken', account.accessToken)
     } catch (error) {
       setState({
         ...state,
